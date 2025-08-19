f145c97b4f845c290cf1f828543840ea27ddc549
Walter9472 <Walter.Mtanda@Student.HTW-Berlin.de>
2025-08-19 23:04:20 +0200

    Add Dev component and integrate it into application startup

---
+++ src/main/java/com/walter/myApp/Dev.java
@@ -0,0 +1,12 @@
+package com.walter.myApp;
+
+import org.springframework.stereotype.Component;
+
+import java.awt.*;
+
+@Component
+public class Dev {
+    public void build(){
+        System.out.println("Working on Awesome Projekt");
+    }
+}
